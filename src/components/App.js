6f6fd545b1374ba78298026b52bda2497f8cf813
Pablo Castro <pablocas-tro1@outlook.com>
2023-08-27 22:10:33 -0400

    Complete Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
+import React, { useEffect, useState } from "react";
 // create your App component here
+function App() {
+  const [image, setImage] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setImage(data.message);
+        setIsLoaded(true);
+      });
+  }, []);
+  if (!isLoaded) return <p>Loading...</p>;
+  return <img src={image} alt="A Random Dog"></img>;
+}
+
+export default App;
